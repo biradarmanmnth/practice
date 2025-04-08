ad7d0228856dd5469cbf4d06c047debe604bd96f
manmnth <biradarmanmnth@gmail.com>
2025-04-08 16:34:23 +0530

    new change

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println(address);
 
         List<String> listAddress=address.stream().flatMap(Collection::stream).toList();
+
         System.out.println(listAddress);
     }
 }
