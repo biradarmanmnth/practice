0e08c158b3cfd99a045531aa6f920e13ee67ff11
manmnth <biradarmanmnth@gmail.com>
2025-04-15 18:06:10 +0530

    array programs

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-import Entity.Employee;
-import Entity.Order;
-import Entity.Product;
-import Exceptions.MyException;
-import Exceptions.MyException;
-import utilityClasses.EmployeeUtil;
-import utilityClasses.ListOfOrders;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        UniqueQuestions.display(null);
+        //UniqueQuestions.display(null);
+        //Find the second highest element in the given array using one for loop
+        //Arrays.secondHighestElement();
+        //Find the first missing positive number from the given array
+        Arrays.firstMissingPositiveNumber();
+
     }
 }
 
